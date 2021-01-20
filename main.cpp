528f24f4aafe04c63e24be1bec693685cff63de9
Giovanni <indirizzoemail@mail.com>
2021-01-20 17:04:54 +0100

    fix indentation

--- main.cpp
+++ main.cpp
@@ -7,34 +7,34 @@
 
 
 bool is_generated_point_in_circle() {
-  int pointX = rand() % WIDTH;
-  int pointY = rand() % HEIGHT;
+    int pointX = rand() % WIDTH;
+    int pointY = rand() % HEIGHT;
 
-  long double distance_from_center = sqrt(pow(pointX - WIDTH / 2, 2) + pow(pointY - HEIGHT / 2, 2));
+    long double distance_from_center = sqrt(pow(pointX - WIDTH / 2, 2) + pow(pointY - HEIGHT / 2, 2));
 
-  return distance_from_center <= 500;
+    return distance_from_center <= 500;
 }
 
 
 int main(const int argc, const char **argv) {
-  srand(time(NULL));
+    srand(time(NULL));
 
-  long long int total_points = 0;
-  long long int circle_points = 0;
-  long double pi = 0;
+    long long int total_points = 0;
+    long long int circle_points = 0;
+    long double pi = 0;
 
-  for (;;) {
-    int is_inside = is_generated_point_in_circle();
-    
-    total_points++;
-    if (is_inside == 1) {
-      circle_points++;
+    for (;;) {
+        int is_inside = is_generated_point_in_circle();
+        
+        total_points++;
+        if (is_inside == 1) {
+        circle_points++;
+        }
+
+        pi = (long double) circle_points / total_points * 4;
+
+        printf("%Lf\n", pi);
     }
-
-    pi = (long double) circle_points / total_points * 4;
-
-    printf("%Lf\n", pi);
-  }
 
   return 0;
 }
